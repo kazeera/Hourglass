e41b97607365f7d3ca003f7b2fff20ffd26d7486
Kazeera Aliar <kazeeraaliar@gmail.com>
2022-08-26 19:31:41 -0400

    Update survival

--- R/survival.R
+++ R/survival.R
@@ -14,29 +14,29 @@ run_surv_analysis <- function(ds, rowAnn1, run, surv_folder = ".") {
     colnames(df)[1:3] <- c("time", "status", "col")
     # Back up variables from original comparisons for later sex comparisons
     df_original <- df
-    
-    # Create output directory 
+
+    # Create output directory
     out_dir <- create_folder(paste(surv_folder, ds$comparison, sep="/"))
     label <- rowAnn1
-    
+
     # Save to file
     filename <- sprintf("%s/%s_survplot.pdf", out_dir, label)
     pdf(filename)
-    
+
     # Plot curve with current strata
     plot_surv_curve(df, label, out_dir)
-    
+
     # If it's a custom analysis (ie groups split into low, int, high), perform binning of 3 groups
     if (all(unique(df$col[!is.na(df$col)]) %in% unlist(LEVELS))) {
       # First bin first and second quartile
       col_lvls <- df$col
       df$col <- bin_vars(col_lvls, LEVELS$i, LEVELS$l) # "intermed" will become "low"
       plot_surv_curve(df, descr = paste(label, "(low+int vs high)"), out_dir)
-      
+
       # Next bin second and third quartile
       df$col <- bin_vars(col_lvls, LEVELS$i, LEVELS$h) # "intermed" will become "high"
       plot_surv_curve(df, descr = paste(label, "(low vs int+high)"), out_dir)
-      
+
       # Now remove int
       col_lvls[col_lvls == LEVELS$i] <- NA # "intermed" will become NA
       df$col <- col_lvls
@@ -44,48 +44,48 @@ run_surv_analysis <- function(ds, rowAnn1, run, surv_folder = ".") {
     }
   })
   dev.off()
-  
+
   # Backup original output folder
   out_dir_orig <- out_dir
-  
+
   # Check whether user wants to divide cohort
   sub_analyses <- strsplit(run$WithinGroup, ";") %>% unlist %>% trimws
   if(length(sub_analyses) == 0 | isTRUE(is.na(sub_analyses))) return()
-  
+
   # For each within group analysis, divide cohort and run hourglass within groups
   for(rowAnn_col in sub_analyses){
     if(!rowAnn_col %in% colnames(ds$rowAnn)) next
-    # Get unique groups 
+    # Get unique groups
     # e.g. If rowAnn_column is "Sex" with unique values NA, "F", "M", groups returns "F" and "M"
     groups <- ds$rowAnn[,rowAnn_col] %>% unique %>% na.omit %>% as.character
-    
+
     # Run hourglass within cohorts
     for (group in groups){
       # Positively select group
       df <- subset_dataframe(df_original, rows_to_keep = ds$rowAnn[,rowAnn_col] == group)
-      
+
       tryCatch({
         # Make survival plots
         # out_dir <- create_folder(paste(out_dir_orig, gsub("-like", "", group)))
         label <- paste(rowAnn1, group)
-        
+
         # Save to file
         filename <- sprintf("%s/%s_survplot.pdf", out_dir_orig, label)
         pdf(filename)
         plot_surv_curve(df, label, out_dir)
-        
+
         # If it's a custom analysis (ie groups split into low, int, high), perform binning of 3 groups
         if (all(unique(df$col[!is.na(df$col)]) %in% unlist(LEVELS))) {
-          
+
           # First bin first and second quartile
           col_lvls <- df$col
           df$col <- bin_vars(col_lvls, LEVELS$i, LEVELS$l) # "intermed" will become "low"
           plot_surv_curve(df, descr = paste(label, "(low+int vs high)"), out_dir)
-          
+
           # Next bin second and third quartile
           df$col <- bin_vars(col_lvls, LEVELS$i, LEVELS$h) # "intermed" will become "high"
           plot_surv_curve(df, descr = paste(label, "(low vs int+high)"), out_dir)
-          
+
           # Now remove int
           col_lvls[col_lvls == LEVELS$i] <- NA # "intermed" will become NA
           df$col <- col_lvls
@@ -109,21 +109,21 @@ plot_surv_curve <- function(df, descr = "", out_dir = ".", save.to.file = F) {
   # library(survminer) #  summarizing and visualizing the results of survival analysis
   # library(viridis) # color palette
   # library(dplyr)
-  
+
   # Rename columns
   colnames(df)[1:3] <- c("time", "status", "col")
-  
+
   tryCatch(
     {
       # Compute KM survival estimate
       fit <- do.call(survfit, list(formula = Surv(time, status) ~ col, data = df))
-      
+
       # Legend labels
       labs <- gsub("col=", "", names(fit$strata)) # "col=high" "col=intermed" "col=low" to "high"     "intermed" "low"
-      
+
       # Make colors
       line_colors <- plasma(n = length(fit$n)+1) %>% .[-(length(.)+1)]
-      
+
       # ggplot theme
       theme <- theme(
         # grid
@@ -145,9 +145,9 @@ plot_surv_curve <- function(df, descr = "", out_dir = ".", save.to.file = F) {
         axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0)))
       suppressWarnings({
         # P-value postiion on x axis
-        pval_x <- (diff(range(unique(fit$time)))-300)/2
-        
-        # Plot  
+        pval_x <- (diff(range(unique(fit$time))))/2 # 20220826 removed -300
+
+        # Plot
         g <- ggsurvplot(fit,
                         # Stats
                         pval = T, # of the Log-Rank test comparing the groups
@@ -162,12 +162,12 @@ plot_surv_curve <- function(df, descr = "", out_dir = ".", save.to.file = F) {
                         title = paste0("Kaplan-Meier, ", out_dir), # plot title
                         # linetype = "col", # Change line type by groups
                         surv.median.line = "hv", # Specify median survival
-                        
-                        ## Colors and themes 
+
+                        ## Colors and themes
                         palette = line_colors, # colors of lines
                         ggtheme =  theme, # theme_cleantable()Change ggplot2 theme
-                        
-                        ## tables: 
+
+                        ## tables:
                         cumevents = T, # add cumulative num of events table
                         risk.table = "absolute", # "abs_pct", # show the absolute number and the percentage of subjects at risk by time
                         # ncensor.plot = TRUE,      # plot the number of censored subjects at time t
@@ -177,7 +177,7 @@ plot_surv_curve <- function(df, descr = "", out_dir = ".", save.to.file = F) {
                         tables.y.text = F, # risk.table.y.text = FALSE, cumevents.y.text = FALSE,# show bars instead of names in text annotations
                         tables.theme = theme_cleantable())
       })
-      
+
       # Save plot to file
       if (save.to.file) {
         # Print to file
